39012f91137971ba8e434eb7215f3eb2d0e8e4fc
Rhain-U-Code-institute <rhainu@hotmail.co.uk>
2025-03-05 13:40:38 +0000

    Add a random functiom with mole class that acts as the mole.

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+
+/**General Variables */
+const squares = document.querySelectorAll('.square')
+const mole = document.querySelector('.mole')
+const timeLeft = document.querySelector('.time-left')
+const score = document.querySelector('.score')
+
+let result = 0
+
+function randomSquare() {
+    squares.forEach(square => {
+        square.classList.remove('mole')
+    })
+
+    let randomPosition = squares[Math.floor(Math.random() * 9)]
+    randomPosition.classList.add('mole')
+
+    hitPosition = randomPosition.id
+}
+
+console.log(randomSquare)
+
+randomSquare()
